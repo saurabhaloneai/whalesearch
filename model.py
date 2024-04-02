86651a830b856eb2ae664e482f69288542ecd4dd
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-04-02 16:58:37 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,14 @@
+#let's start the on how we take the inputs 
+
+#TODO:
+#1. inputs 
+#2. figure out the input processing 
+#3. build the retrievel stuff
+#4. build the ranker 
+#5. output processing 
+
+
+##INPUTS 
+
+
+
