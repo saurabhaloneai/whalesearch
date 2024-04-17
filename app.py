696fbaa4452dcfc2d2f1668e8cb3df66e183de0d
saurabhaloneai <saurabhaloney85@gmail.com>
2024-04-17 09:00:05 +0530

    handle the api thing

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 # TODO:
 # 1. Build the model from coursera and lightining course and combine it
+
+import os
+
+import cohere
+
+API_KEY = "Len6EF6Rh8Ym0NjDPibrhBWIMJVsrpo7kIFm3VjP"
+
+client = cohere.Client(os.environ["Len6EF6Rh8Ym0NjDPibrhBWIMJVsrpo7kIFm3VjP"])
