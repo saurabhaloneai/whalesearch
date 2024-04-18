a063f0fe6334e292af925aff93e7e9b446157113
saurabh <99719265+saurabhaloneai@users.noreply.github.com>
2024-04-18 23:27:15 +0530

    Update image.py

--- image.py
+++ image.py
@@ -9,12 +9,7 @@ from lightning_cloud.utils.data_connection import add_s3_connection
 
 RAW_DATA_DIR = "/teamspace/s3_connections/imagenet-1m-template/raw/"
 
-# 1. Add the raw dataset to your teamspace
-
-# TODO:
-# 1. get the local data to seach thorugh it
-# 2. add the data into s3
-# 3. try to get online data don't know how but might work
+#
 
 # 2. Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
